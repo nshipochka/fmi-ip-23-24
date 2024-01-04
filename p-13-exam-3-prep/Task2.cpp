b18167d013a846ce064397e7fdcd6ec68bcac948
nshipochka <nshipochka@gmail.com>
2024-01-04 17:37:39 +0200

    Add solutions to tasks 2 & 3

---
+++ p-13-exam-3-prep/Task2.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+
+bool diff_digits(int curr_digit, int rest_of_n) {
+	if (!rest_of_n)
+		return true;
+
+	if (curr_digit == rest_of_n % 10)
+		return false;
+
+	return diff_digits(curr_digit, rest_of_n / 10) // цифрата, за която сега проверяваме, дали се среща по-натам в числото
+		&& diff_digits(rest_of_n % 10, rest_of_n / 10); // проверяваме за следващата цифра
+}
+
+bool diff_digits(int n) {
+	return diff_digits(n % 10, n / 10);
+}
+
+int main() {
+	std::cout << std::boolalpha << diff_digits(102) << std::endl;
+	std::cout << std::boolalpha << diff_digits(101) << std::endl;
+	return 0;
+}
---
+++ p-13-exam-3-prep/Task3.cpp
@@ -0,0 +1,75 @@
+﻿#include<iostream>
+
+void sort(int* arr, size_t sz) {
+	for (size_t i = 0; i < sz - 1; ++i) {
+		size_t min_ind = i;
+
+		for (size_t j = i + 1; j < sz; ++j) {
+			if (arr[min_ind] < arr[j])
+				min_ind = j;
+		}
+
+		if (min_ind != i)
+			std::swap(arr[min_ind], arr[i]);
+	}
+}
+
+bool are_permutations(int* arr1, int* arr2, size_t sz) {
+	sort(arr1, sz);
+	sort(arr2, sz);
+
+	for (size_t i = 0; i < sz; ++i) {
+		if (arr1[i] != arr2[i])
+			return false;
+	}
+
+	return true;
+}
+
+bool has_permuting_rows(int** matrix, size_t sz) {
+	for (size_t i = 0; i < sz - 1; ++i) 
+		for (size_t j = i + 1; j < sz; ++j)
+			if (are_permutations(matrix[i], matrix[j], sz))
+				return true;
+	return false;
+}
+
+bool has_permuting_diags(int** matrix, size_t sz) {
+	int* principal_diag = new int[sz];
+	int* secondary_diag = new int[sz];
+	size_t p_sz = 0;
+	size_t s_sz = 0;
+
+	for (size_t i = 0; i < sz; ++i) {
+		for (size_t j = 0; j < sz; ++j) {
+			if (i == j)
+				principal_diag[p_sz++] = matrix[i][j];
+			if (i == sz - 1 - j)
+				secondary_diag[s_sz++] = matrix[i][j];
+		}
+	}
+
+	bool result = are_permutations(principal_diag, secondary_diag, sz);
+
+	delete[] principal_diag;
+	delete[] secondary_diag;
+
+	return result;
+}
+
+void print(int* arr, size_t sz) {
+	for (size_t i = 0; i < sz; ++i)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+}
+
+void print(int** matrix, size_t sz) {
+	for (size_t i = 0; i < sz; ++i)
+		print(matrix[i], sz);
+}
+
+int main() {
+	
+
+	return 0;
+}
